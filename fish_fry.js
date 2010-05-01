208746ee86cabc1e19b3cdecc3c26a9fb5220de5
Matt Hanlon <matthanlon@gmail.com>
2010-05-01 15:13:39 -0700

    Have fish fry grep reddit for URLs when they are submitted and give a snide remark about STORPHA

--- fish_fry.js
+++ fish_fry.js
@@ -1,3 +1,5 @@
+var sys = require('sys');
+var http = require('http');
 var jerk = require('./lib/Jerk/lib/jerk');
 
 var options =
@@ -14,5 +16,31 @@ jerk(function(j) {
   j.watch_for(/^(.+) are silly$/, function(message) {
     message.say(message.user + ': ' + message.match_data[1] + ' are NOT SILLY.');
   });
+
+  var urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
+  j.watch_for(urlRegex, function(message){
+    var reddit = http.createClient(80, 'www.reddit.com');
+    var request = reddit.request('GET', '/', {'host': 'www.reddit.com'});
+    request.addListener('response', function (response) {
+      if(response.statusCode != 200){
+        message.say(message.user + ': How did you find that link when reddit is down?');
+      }else{
+        var page = '';
+        response.setEncoding('utf8');
+        response.addListener('data', function (chunk) {
+          page += chunk;
+        });
+        response.addListener('end', function(){
+          if(page.indexOf(message.match_data[0]) > -1){
+            message.say(message.user + ': thanks jackass we all read reddit.');
+          }else{
+            message.say(message.user + ': cool link, bro');
+          }
+        });
+      }
+    });
+    request.end();
+  });
+ 
 
 }).connect(options);
