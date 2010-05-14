5ef6622dab208b8c26e6202bd78e38250235444b
wamberg <wamberg@technushi.com>
2010-05-13 21:44:38 -0400

    Added request/response debug messages to stderr for deed logging and fetching reddit.com.

--- fish_fry.js
+++ fish_fry.js
@@ -1,6 +1,9 @@
-var sys = require('sys');
+// node libraries
 var http = require('http');
+var sys = require('sys');
 var querystring = require('querystring');
+
+// third-party libraries
 var jerk = require('./lib/Jerk/lib/jerk');
 var base64 = require('./lib/base64');
 try {
@@ -9,9 +12,19 @@ try {
   throw new Error('See the README and define local_settings.js');
 }
 
+// runtime configuration
+var fish_fry = {debug: false};
+
+process.argv.forEach(function (val, index, array) {
+  if (val.toLowerCase() === 'debug') {
+    fish_fry.debug = true
+  }
+});
+
 jerk(function(j) {
   j.watch_for('',function(message) {
-    // TODO should whitelist names. didn't bother to look how this is done in old fish_fry
+    // TODO should whitelist names
+    // didn't bother to look how this is done in old fish_fry
     var deeds = http.createClient(80, settings.valhalla_options.host);
     var headers = {'host': settings.valhalla_options.host};
     if(settings.valhalla_options.http_basic_auth){
@@ -19,9 +32,25 @@ jerk(function(j) {
         + base64.encode(settings.valhalla_options.user + ':' 
         + settings.valhalla_options.pass);
     }
-    var body = querystring.stringify({'deed': {'speaker': message.user, 'performed_at': (new Date()).toString(), 'text': message.text.join(' ')}});
+    var body = querystring.stringify(
+      {'deed': {
+        'speaker': message.user,
+        'performed_at': (new Date()).toString(),
+        'text': message.text.join(' ')}});
     headers['Content-Length'] = body.length;
     var request = deeds.request('POST', '/deeds.json', headers);
+    if (fish_fry.debug) {
+      sys.debug('DEED REQUEST:' + sys.inspect(request));
+      request.addListener('response', function (response) {
+        sys.debug('DEED RESPONSE STATUS: ' + response.statusCode);
+        sys.debug('DEED RESPONSE HEADERS: ' + JSON.stringify(
+            response.headers));
+        response.setEncoding('utf8');
+        response.addListener('data', function (chunk) {
+          sys.debug('DEED RESPONSE BODY: ' + chunk);
+        });
+      });
+    }
     request.write(body);
     request.end();
   });
@@ -30,6 +59,18 @@ jerk(function(j) {
   j.watch_for(urlRegex, function(message){
     var reddit = http.createClient(80, 'www.reddit.com');
     var request = reddit.request('GET', '/', {'host': 'www.reddit.com'});
+    if (fish_fry.debug) {
+      sys.debug('STORPHA REQUEST:' + sys.inspect(request));
+      request.addListener('response', function (response) {
+        sys.debug('STORPHA RESPONSE STATUS: ' + response.statusCode);
+        sys.debug('STORPHA RESPONSE HEADERS: ' + JSON.stringify(
+            response.headers));
+        response.setEncoding('utf8');
+        response.addListener('data', function (chunk) {
+          sys.debug('STORPHA RESPONSE BODY: ' + chunk);
+        });
+      });
+    }
     request.addListener('response', function (response) {
       if(response.statusCode != 200){
         message.say(message.user + ': How did you find that link when reddit is down?');
